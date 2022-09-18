d5a38b2c7b76668e26b995cbc3efbaa497ff0698
iisrafil <you@example.com>
2022-09-18 20:07:16 +0600

    first

--- api/views.py
+++ api/views.py
@@ -1,21 +1,33 @@
 from django.http import JsonResponse
+from rest_framework.viewsets import ViewSet
 
 from api.models import Device, Finger
+from api.serializers import FingerSerializer
 from scanner.models import Registered
 from scanner.utility import scanner
 
 
-# Create your views here.
-def home(request):
-    device = Device.objects.get()
-    finger = Finger.objects.get()
+class UploadViewSet(ViewSet):
+    serializer_class = FingerSerializer
 
-    result, time, percent = scanner(finger.image.path, device.depth);
-    if percent:
-        user = Registered.objects.get(finger="Registered/" + result)
-    return JsonResponse({
-        "user": user.id,
-        "Name": user.name,
-        "match": result,
-        "time": time,
-    })
+    def create(self, request):
+        device = request.data.get('device')
+        device = Device.objects.get(pk=device)
+        file_uploaded = request.FILES.get('image')
+        finger = Finger(device=device, image=file_uploaded)
+        finger.save()
+        result, time, percent = scanner(finger.image.path, device.depth);
+        if percent > 60:
+            user = Registered.objects.get(finger="Registered/" + result)
+        else:
+            return JsonResponse({
+                "match": False,
+                "finger": finger.pk,
+            })
+        return JsonResponse({
+            "match": True,
+            "user": user.id,
+            "Name": user.name,
+            "finger": result,
+            "time": time,
+        })
--- fingerScanner/urls.py
+++ fingerScanner/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from api.views import home
+from api.views import UploadViewSet
+
+router = routers.DefaultRouter()
+router.register(r'finger/', UploadViewSet, basename="upload")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name="home"),
+    path('api/', include(router.urls)),
 ]
