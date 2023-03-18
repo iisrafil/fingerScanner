347cada49adc5c6cdb6011578b43404e943ced02
S-K-Sami <sami8907456312@gmail.com>
2023-03-18 16:23:06 +0600

    Back Init

--- fingerScanner/settings.py
+++ fingerScanner/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-m**a1yr(!fq1h7zva&zs=r-317)al*80ydgxb=0u$1#w=xal&@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['python-webapp-54321.azurewebsites.net','smartcarbd.com','www.smartcarbd.com', 'www.python-webapp-54321.azurewebsites.net', '127.0.0.1']
+ALLOWED_HOSTS = ['python-webapp-54321.azurewebsites.net','smartcarbd.com','www.smartcarbd.com', 'www.python-webapp-54321.azurewebsites.net', '127.0.0.1', "192.168.0.101"]
 
 
 # Application definition
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     "front.apps.FrontConfig",
+    'api.apps.ApiConfig',
+    'scanner.apps.ScannerConfig',
     "debug_toolbar",
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- scanner/models.py
+++ scanner/models.py
@@ -3,13 +3,13 @@ from django.db import models
 from api.models import Device
 
 
-class Finger(models.Model):
+class FingerScn(models.Model):
     device = models.ForeignKey(Device, on_delete= models.CASCADE)
     time = models.DateTimeField(auto_now_add=True, blank=True)
     image = models.ImageField(upload_to='finger_scan')
 
 
 class Registered(models.Model):
-    id = models.CharField(max_length=50)
+    id = models.CharField(max_length=50, primary_key=True)
     name = models.CharField(max_length=100)
     finger = models.ImageField(upload_to="Registered")
